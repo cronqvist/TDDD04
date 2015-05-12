66daea1246d6f3e0c46daadb6669e282d0885dd5
mattias@cronqvist.eu <mattias@cronqvist.eu>
2015-05-12 12:33:10 +0200

    added first test to rectangle

---
+++ TDDD04_lab2_VS2013/BlackLagoon.Tests/RectangleTests.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "Rectangle.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BlackLagoontests
+{
+	TEST_CLASS(PlayerTests)
+	{
+
+	public:
+		geometric::Rectangle<float>* _rect1;
+		geometric::Rectangle<float>* _rect2;
+
+		TEST_METHOD_INITIALIZE(setUp)
+		{
+			_rect1 = new geometric::Rectangle<float>();
+			_rect2 = new geometric::Rectangle<float>();
+		}
+
+		TEST_METHOD_CLEANUP(tearDown)
+		{
+			//delete _rect1;
+			//delete _rect2;
+		}
+
+		TEST_METHOD(testRectangleOverlapFromRight)
+		{
+			bool result = false;
+
+			geometric::Rectangle<float>* _rect1 = new geometric::Rectangle<float>(0, 1800, 30, 1860);
+			geometric::Rectangle<float>* _rect2 = new geometric::Rectangle<float>(3.24, 1748.43, 47.24, 1826.43);
+
+			result = _rect1->overlap(*_rect2);
+			Assert::AreEqual(true, result, L"Rectangle should overlap but did not");
+		}
+
+	};
+}
